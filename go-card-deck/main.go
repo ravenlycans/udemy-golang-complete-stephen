8dc9302a20ccc179dc3a976841acc645a98b6915
Kim Andersen <wolfy@lycans.dk>
2024-12-04 14:23:37 +0100

    End of Lecture 16

--- go-card-deck/main.go
+++ go-card-deck/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	//	var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	fmt.Println(card)
+	card := newCard()
 
-	card = "Ace of Clubs"
-	fmt.Print(card)
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }

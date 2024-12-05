e67de8d4a0cb9278578d8c4255e7bf69a3799b33
Kim Andersen <wolfy@lycans.dk>
2024-12-05 14:04:49 +0100

    End of lecture 54

--- go-maps/main.go
+++ go-maps/main.go
@@ -3,12 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#FF0000",
-		"green": "#00FF00",
-		"blue":  "#0000FF",
-		"weird": "#4BF745",
-	}
+	//	var colors map[string]string
+
+	//	colors := make(map[string]string)
+
+	/*
+		colors := map[string]string{
+			"red":   "#FF0000",
+			"green": "#00FF00",
+			"blue":  "#0000FF",
+			"weird": "#4BF745",
+		}
+	*/
+	/*
+		colors["red"] = "#FF0000"
+		colors["green"] = "#00FF00"
+		colors["blue"] = "#0000FF"
+		colors["white"] = "#FFFFFF"
+		colors["weird"] = "#4BF745"
+	*/
+	colors := make(map[int]string)
+
+	colors[10] = "white"
+
+	fmt.Println(colors)
+
+	delete(colors, 10)
 
 	fmt.Println(colors)
 }

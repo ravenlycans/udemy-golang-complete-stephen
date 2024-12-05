68740e03f7f69da4ea61afb37177e36431263995
Kim Andersen <wolfy@lycans.dk>
2024-12-05 14:10:21 +0100

    End of lecture 55

--- go-maps/main.go
+++ go-maps/main.go
@@ -3,32 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	//	var colors map[string]string
+	colors := make(map[string]string)
+	colors["red"] = "#FF0000"
+	colors["green"] = "#00FF00"
+	colors["blue"] = "#0000FF"
+	colors["white"] = "#FFFFFF"
+	colors["black"] = "#000000"
+	colors["weird"] = "#4BF745"
 
-	//	colors := make(map[string]string)
+	printMap(colors)
+}
 
-	/*
-		colors := map[string]string{
-			"red":   "#FF0000",
-			"green": "#00FF00",
-			"blue":  "#0000FF",
-			"weird": "#4BF745",
-		}
-	*/
-	/*
-		colors["red"] = "#FF0000"
-		colors["green"] = "#00FF00"
-		colors["blue"] = "#0000FF"
-		colors["white"] = "#FFFFFF"
-		colors["weird"] = "#4BF745"
-	*/
-	colors := make(map[int]string)
-
-	colors[10] = "white"
-
-	fmt.Println(colors)
-
-	delete(colors, 10)
-
-	fmt.Println(colors)
+func printMap(m map[string]string) {
+	for col, hex := range m {
+		fmt.Printf("Hex code for %s is %s\n", col, hex)
+	}
 }

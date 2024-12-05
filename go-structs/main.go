dcb8807dbf28101abc063356e4a97454c4bc0fb1
Kim Andersen <wolfy@lycans.dk>
2024-12-05 12:50:22 +0100

    End of lecture 43

--- go-structs/main.go
+++ go-structs/main.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
-	
+	jane := person{"Jane", "Doe"}
+	alex := person{firstName: "Alex", lastName: "Anderson"}
+
+	fmt.Printf("%s %s\n", jane.firstName, jane.lastName)
+	fmt.Printf("%s %s\n", alex.firstName, alex.lastName)
 }

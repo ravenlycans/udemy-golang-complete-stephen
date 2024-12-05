6f6df1f770d2c630c7cad7f2a62e23353c3e24fe
Kim Andersen <wolfy@lycans.dk>
2024-12-05 11:17:47 +0100

    End of lecture 36

--- go-card-deck/deck_test.go
+++ go-card-deck/deck_test.go
@@ -2,10 +2,26 @@ package main
 
 import "testing"
 
-func TestNewDeck(t *testing.T) {
+func TestNewDeckCount(t *testing.T) {
 	deck := newDeck()
 
 	if len(deck) != 52 {
 		t.Errorf("Expected 52 cards in deck, got %d", len(deck))
+	}
+}
+
+func TestNewDeckFirstCard(t *testing.T) {
+	deck := newDeck()
+
+	if deck[0] != "Ace of Clubs" {
+		t.Errorf("Expected first card to be Ace of Clubs, got %s", deck[0])
+	}
+}
+
+func TestNewDeckLastCard(t *testing.T) {
+	deck := newDeck()
+
+	if deck[51] != "King of Spades" {
+		t.Errorf("Expected last card to be King of Spades, got %s", deck[51])
 	}
 }

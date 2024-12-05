a1adbc0d4df5789ca52472665bc7bad5b479eba4
Kim Andersen <wolfy@lycans.dk>
2024-12-05 14:41:31 +0100

    End of lecture 58

--- go-interfaces/main.go
+++ go-interfaces/main.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printSpanishGreeting(sb)
+}
+
+func (englishBot) getGreeting() string {
+	// Very custom logic for generating an english greeting
+	return "Greetings!!"
+}
+
+func (spanishBot) getGreeting() string {
+	// Very custom logic for generating an spanish greeting
+	return "Hola!!"
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printSpanishGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}

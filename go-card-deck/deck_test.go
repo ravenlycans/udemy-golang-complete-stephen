b0106e5c9848d600c3d677185329e98bd390da28
Kim Andersen <wolfy@lycans.dk>
2024-12-05 11:34:58 +0100

    End of lecture 37

--- go-card-deck/deck_test.go
+++ go-card-deck/deck_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"slices"
+	"testing"
+)
 
 func TestNewDeckCount(t *testing.T) {
 	deck := newDeck()
@@ -23,5 +28,70 @@ func TestNewDeckLastCard(t *testing.T) {
 
 	if deck[51] != "King of Spades" {
 		t.Errorf("Expected last card to be King of Spades, got %s", deck[51])
+	}
+}
+
+func TestDeckShuffle(t *testing.T) {
+	deck := newDeck()
+	deck.shuffle()
+
+	deckOrig := newDeck()
+
+	if deckOrig.toString() == deck.toString() {
+		t.Errorf("Expected deck to be shuffled, got unshuffled deck string %s", deck.toString())
+	}
+}
+
+func TestDeckSaveToFile(t *testing.T) {
+	_ = os.Remove("test.txt")
+
+	deck := newDeck()
+	err := deck.saveToFile("test.txt")
+
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
+
+	_, err = os.ReadFile("test.txt")
+
+	if errors.Is(err, os.ErrNotExist) {
+		t.Errorf("Expected file to be created, got %s", err)
+	}
+
+	_ = os.Remove("test.txt")
+}
+
+func TestNewDeckFromFile(t *testing.T) {
+	_ = os.Remove("test.txt")
+
+	deck := newDeck()
+	err := deck.saveToFile("test.txt")
+
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
+
+	deckFromFile := newDeckFromFile("test.txt")
+	if deck.toString() != deckFromFile.toString() {
+		t.Errorf("Expected deck to be equal to deck from file, got %s", deck.toString())
+	}
+
+	_ = os.Remove("test.txt")
+}
+
+func TestDeckDeal(t *testing.T) {
+	deck := newDeck()
+	hand, remainingDeck := deck.deal(5)
+
+	if len(hand) != 5 {
+		t.Errorf("Expected 5 cards in hand, got %d", len(hand))
+	}
+
+	if len(remainingDeck) != 47 {
+		t.Errorf("Expected 47 cards in remaining deck, got %d", len(remainingDeck))
+	}
+
+	if slices.Contains(remainingDeck, hand[0]) {
+		t.Errorf("Expected hand[0] to be removed from remaining deck, got %s", remainingDeck)
 	}
 }

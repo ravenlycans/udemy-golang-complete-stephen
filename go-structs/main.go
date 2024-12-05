dcce12ea3e5c2eaf1e0f35a4fad71c01ea15cb11
Kim Andersen <wolfy@lycans.dk>
2024-12-05 13:03:00 +0100

    End of lecture 45

--- go-structs/main.go
+++ go-structs/main.go
@@ -2,21 +2,28 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	jane := person{"Jane", "Doe"}
+	jane := person{"Jane", "Doe", contactInfo{"jane@test.com", 123345}}
 
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	alex := person{firstName: "Alex", lastName: "Anderson", contact: contactInfo{email: "alex@test.com", zipCode: 243562}}
 
 	john := person{}
 	john.firstName = "John"
 	john.lastName = "Doe"
+	john.contact = contactInfo{email: "john@test.com", zipCode: 123456}
 
-	fmt.Printf("%s %s\n", jane.firstName, jane.lastName)
-	fmt.Printf("%s %s\n", alex.firstName, alex.lastName)
-	fmt.Printf("%s %s\n", john.firstName, john.lastName)
+	fmt.Printf("%s %s, email: %s, zipCode: %d\n", jane.firstName, jane.lastName, jane.contact.email, jane.contact.zipCode)
+	fmt.Printf("%s %s, email: %s, zipCode: %d\n", alex.firstName, alex.lastName, alex.contact.email, alex.contact.zipCode)
+	fmt.Printf("%s %s, email: %s, zipCode: %d\n", john.firstName, john.lastName, john.contact.email, john.contact.zipCode)
 }

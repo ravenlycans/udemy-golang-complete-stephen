84728b7859095c0e261462d5a4c461a2b71c217d
Kim Andersen <wolfy@lycans.dk>
2024-12-05 13:59:23 +0100

    End of lecture 53

---
+++ go-maps/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#00FF00",
+		"blue":  "#0000FF",
+		"weird": "#4BF745",
+	}
+
+	fmt.Println(colors)
+}

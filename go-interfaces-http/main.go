c0338211ff6e2fca9c3ad57b9b9ea7be332eb406
Kim Andersen <wolfy@lycans.dk>
2024-12-06 10:35:37 +0100

    End of lecture 63,64,65,66,67,68 and 69

--- go-interfaces-http/main.go
+++ go-interfaces-http/main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
-	resp, err := http.Get("https://www.google.com")
+	r, err := http.Get("http://www.google.com")
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(resp)
+	bs := make([]byte, 1024)
+	bs, err = io.ReadAll(r.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(bs))
+	fmt.Printf("Read %d bytes\n", len(bs))
 }
